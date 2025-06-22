ee23a5e95d032641109090b779bfe9438b82452e
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-06-22 21:09:54 +0630

    :sparkles: feat(finalize set up):

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { QueryProviders } from './lib/QueryProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryProviders>
+      <RouterProvider router={router} />
+    </QueryProviders>
   </StrictMode>
 );
